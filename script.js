e36140c7d5ef2cb7fc80aa98979b2ba69ec6cc24
Jasim <m.dev1@eriyada.com>
2021-03-10 15:41:32 +0400

    Initial completion commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const laoder = document.getElementById("loader");
+
+let apiQuotes = [];
+
+//Show loading
+function loading() {
+  laoder.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Show loading
+function loadingComplete() {
+  laoder.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+//Show new quote
+function newQuote() {
+  loading();
+  //Pick a randrom quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  //Set quote and hide loader
+  quoteText.textContent = quote.text;
+  loadingComplete();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch error here
+    loadingComplete();
+  }
+}
+
+// Tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuotes();
